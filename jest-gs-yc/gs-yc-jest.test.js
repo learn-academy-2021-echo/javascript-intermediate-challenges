b0ab1fe4734e96752a602db4c6937cd80be88cae
gluay88 <p.wantagaew@gmail.com>
2021-12-08 22:51:33 -0800

    Done no. 1-11

--- jest-gs-yc/gs-yc-jest.test.js
+++ jest-gs-yc/gs-yc-jest.test.js
@@ -26,73 +26,151 @@
 //     expect(areYouExhausted("no")).toEqual("keep going")
 //   })
 // })
+//
 // const areYouExhausted = (string) => {
 //   if(string === "yes"){
 //   return "relax"
-// } else if(string === "no"){
+//   } else if(string === "no"){
 //   return "keep going"
-// }
+//   }
 // }
 
+//3
 // Write the test for a function that returns "in budget" if a price is lower than $300.
 // Write the function that will make the test pass.
 //
 // describe("areYouInBudget", () => {
-//
-//   it("returns in budget if lower than $300", () => {
-//
-//     expect(areYouInBudget( < 300 )).toEqual("in budget")
-//     expect(areYouInBudget( > 300)).toEqual("work harder")
+//   it("input number then return a string", () => {
+//     expect(areYouInBudget(290)).toEqual("in budget")
 //   })
 // })
-// const areYouInBudget = (value) => {
-//   if(value < 300){
-//   return "in budget"
-// } else if(value > 300){
-//   return "work harder"
+//
+// const areYouInBudget = (budget) => {
+//   if(budget < 300) {
+//     return "in budget"
+//   } else {
+//     return "work harder"
+//   }
 // }
-// }      // come back later !!!!!!!!!
 
+//4
 // Write the test for a function that takes in two arguments(numbers) and returns the smaller number.
-// Write the function that will make the test pass.
-
-// describe("smallerNumber", () => {
-//
-//   it("returns the smaller number", () => {
-//
-//     expect(smallerNumber("")).toEqual("in budget")
-//     expect(smallerNumber("value > 300")).toEqual("work harder")
+// describe("smallerNum", (num1, num2) => {
+//   it("takes two numbers and compare them and then returns the smaller number", () => {
+//     expect(smallerNum(num1 > num2)).toEqual(num1)
+//     expect(smallerNum(num2 > num1)).toEqual(num2)
 //   })
 // })
-// const areYouInBudget = (value) => {
-//   if(value < 300){
-//   return "in budget"
-// } else if(value > 300){
-//   return "work harder"
-// }
 //
+// const smallerNum = (num1, num2) => {
+//   if(num1 > num2) {
+//     return num1
+//   } else {
+//     return num2
+//   }
+// }
 
+//5
+//Write the test for a function that takes in one argument(number) and returns whether the number is odd.
+// describe("oddNum", (number) => {
+//   it("takes a number and return a tring", () => {
+//     expect(oddNum(number%2===0)).toEqual("odd")
+//
+//   })
+// })
+// const oddNum = (number) => {
+//   if(number % 2 === 0) {
+//     return "odd"
+//   } else {
+//     return "even"
+//   }
+// }
 
-
-
-
-
-
-// Write the test for a function that takes in one argument(number) and returns whether the number is odd.
-// Write the function that will make the test pass.
+//6
 // Write the test for a function that takes in a fruit and returns "yellow" if the argument is banana, "red" if apple and "purple" if grape.
-// Write the function that will make the test pass.
-// Write the test for a function called rick that returns "Morty".
-// Write the function that will make the test pass.
+
+// describe("fruitColors", (fruit) => {
+//   it("takes a type of fruits and return the colors of them", () => {
+//     expect(fruitColors("banana")).toEqual("yellow")
+//   })
+// })
+//
+// const fruitColors = (fruit) => {
+//   if(fruit==="banana") {
+//     return "yellow"
+//   } else if(fruit === "apple"){
+//     return "red"
+//   } else if(fruit === "grape") {
+//     return "purple"
+//   } else {
+//     return "not a fruit"
+//   }
+// }
+
+
+//7 Write the test for a function called rick that returns "Morty".
+// describe("buddies", (character) => {
+//   it("takes rick and returns Morty", () => {
+//     expect(buddies("rick")).toEqual("Morty")
+//   })
+// })
+//
+// const buddies = (character) => {
+//   if(character === "rick"){
+//     return "Morty"
+//   }
+// }
+
+
+//8
 // Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen.
-// Write the function that will make the test pass.
+// describe("greeter", (name) => {
+//   it("takes name and returns Hello! name", () => {
+//     expect(greeter(name)).toEqual("Hello! name")
+//   })
+// })
+//
+// const greeter = (name) => {
+//   return `Hello! name`
+// }
+
+//9
 // Write the test for a function called oddOrEven that takes a number as an argument and logs whether the number is odd or even.
-// Write the function that will make the test pass.
+// describe("oddOrEven", (numbers) => {
+//   it("takes numbers returns the value odd or even", () => {
+//     expect(oddOrEven(numbers%2===0)).toEqual("even")
+//     expect(oddOrEven(numbers%2!==0)).toEqual("odd")
+//   })
+// })
+//
+// const oddOrEven = (numbers) => {
+//   if(numbers % 2 === 0) {
+//     return "even"
+//   } else {
+//     return "odd"
+//   }
+// }
+
+//10
 // Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2.
-// Write the function that will make the test pass.
-// Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other.
-// Write the function that will make the test pass.
-// Write the test for a function called divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
-// Write the function that will make the test pass.
-// Write the test for a function called fizzbuzz. If a number is a multiple of 3, replace it with the word 'fizz'. If a number is a multiple of five, replace it with the word 'buzz'. If a number is a multiple of both 3 and 5, replace it with 'fizzbuzz'.
-// Write the function that will make the test pass.
+// describe("doubler", (numbers) => {
+//   it("takes numbers returns the value multiplied by 2", () => {
+//     expect(doubler(numbers * 2)).toEqual(numbers * 2)
+//   })
+// })
+//
+// const doubler = (numbers) => {
+//   return numbers * 2
+// }
+
+//11
+// // Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other.
+// describe("multiply", (num1, num2) => {
+//   it("takes two numbers returns the those two numbers multiplied by the other", () => {
+//     expect(multiply(num1 * num2)).toEqual(num1 * num2)
+//   })
+// })
+//
+// const multiply = (num1, num2) => {
+//   return num1 * num2
+// }
