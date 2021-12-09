501b856433b3af4b55b80b00bc65647f1ecbaf89
DeonFranklin <deondeefranklin@gmail.com>
2021-12-09 11:37:52 -0800

    Learned more stuff and made some correections.

--- jest-pavan-deon/jest.test.js
+++ jest-pavan-deon/jest.test.js
@@ -1,46 +1,71 @@
-describe("areYouTired", () => {
-    it("returns drink coffee or keep working based on input", () => {
-      expect(areYouTired("yes")).toEqual("drink coffee")
-      expect(areYouTired("no")).toEqual("keep working")
+// describe("areYouTired", () => {
+//     it("returns drink coffee or keep working based on input", () => {
+//       expect(areYouTired("yes")).toEqual("drink coffee")
+//       expect(areYouTired("no")).toEqual("keep working")
+//     })
+// })
+// const areYouTired = (string) => {
+//     if(string === "yes"){
+//       return "drink coffee"
+//       console.log("1")
+//     }else if(string === "no"){
+//     return( "keep working")
+//       console.log("2")
+//     }
+// }
+
+// describe ("areYouStressed", ()=>{
+//     it("returns relax or or keep goin", ()=>{
+//         expect(areYouStressed("yes")).toEqual("relax")
+//         expect(areYouStressed("no")).toEqual("keep goin")
+//     })
+//})
+
+// const areYouStressed = (string) => {
+//     if(string === "yes"){
+//       return "relax"
+//     } else if(string === "no") {
+//       return ("keep goin")
+//     }
+//}
+
+// describe ("underThree", ()=>{
+//     it("returns in budget or too expensive", ()=>{
+//         expect(underThree("yes")).toEqual("in budget")
+//         expect(underThree("no")).toEqual("too expensive")
+//     })
+// })
+// const underThree = (string) => {
+//     if(string === "yes"){
+//       return "in budget"
+//     } else if(string === "no") {
+//       return ("too expensive")
+//     }
+// }
+// describe ("underThree", ()=>{
+//     it("returns in budget or too expensive", ()=>{
+//         expect(underThree("yes")).toEqual("in budget")
+//         expect(underThree("no")).toEqual("too expensive")
+//     })
+// })
+// const underThree = (string) => {
+//     if(string === "yes"){
+//       return "in budget"
+//     } else if(string === "no") {
+//       return ("too expensive")
+//     }
+// }
+describe ("lowNum", ()=>{
+    it("returns the lower number", ()=>{
+        expect(lowNum(3, 9)).toEqual(3)
+        expect(lowNum(9, 3)).toEqual(3)
     })
-  })
-
-const areYouTired = (string) => {
-
-    if(string === "yes"){
-      return "drink coffee"
-      console.log("1")
-    }{ elseif(string === "no") 
-    }return( "keep on truckin' ")
-     { console.log("2")
+})
+const lowNum = (3,9) => {
+    if(3 > 9){
+      return 3
+    } else if(3 < 9) {
+      return 9
     }
 }
 
-// const areYouStressed = (string) => {
-//     if(string === "yes"){
-//       return "relax"
-//     } else if(string === "no") {
-//       return "keep goin"
-//     }
-//   }
-
-// console.log(areYouStressed,"yes")
-
-// const areYouStressed = (string) => {
-//     if(string === "yes"){
-//       return "relax"
-//     } else if(string === "no") {
-//       return "keep goin"
-//     }
-//   }
-
-// console.log(areYouStressed,["yes"])
-
-
-// const areYouHungry = (string) => {
-//     if(string === "yes"){
-//       return "eat food"
-//     } else if(string === "no") {
-//       return "keep working"
-//     }
-//   }
