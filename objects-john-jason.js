989a566b24f4a082aeaf2cbfaccfdcc43974b55d
hawk198723 <hawk198723@gmail.com>
2021-12-10 10:38:31 -0800

    still working on it

--- objects-john-jason.js
+++ objects-john-jason.js
@@ -4,10 +4,9 @@ var person = {
   firstName: "Arthur",
   lastName: "Dent",
   homePlanet: "Earth",
-  getData: homePlanet(){
-    return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
+  getData: function(){
+    return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}.`
     }
-
 }
 
 
@@ -21,29 +20,58 @@ console.log(person.lastName)
 console.log(person.getData())
 
 // Consider this variable:
-// var product = {
-//   name: "chair", price: 14.99
-// }
+var product = {
+  name: "chair",
+  price: 14.99
+
+}
+
+
 // Write a function called describeProduct takes product as an argument and logs "The product is a chair. It costs $14.99".
+
+const describeProduct = (object) => {
+  return `The product is a ${object.name}. It costs ${object.price}`
+}
+console.log(describeProduct(product));
+
 // Consider this variable:
-// var lunch = {
-//   name: "PB and Banana",
-//   type: "sandwich",
-//   ingredients: ["bread", "peanut butter", "banana"]
-// }
+var lunch = {
+  name: "PB and Banana",
+  type: "sandwich",
+  ingredients: ["bread", "peanut butter", "banana"],
+  formula: function(){
+    return `The ingredients for a ${this.name} ${this.type} are ${this.ingredients[0]}, ${this.ingredients[1]}, and ${this.ingredients[2]}.`
+  }
+}
 // Write the code that accesses the ingredients property.
 // Write the code that access the 3rd ingredient of the lunch object.
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+
+console.log(lunch.ingredients);
+console.log(lunch.ingredients[2]);
+const myLunch = (object) => {
+  return `The ingredients for a ${object.name} ${object.type} are ${lunch.ingredients[0]}, ${lunch.ingredients[1]}, and ${lunch.ingredients[2]}.`
+}
+console.log(myLunch(lunch));
+console.log(lunch.formula());
+
 // Consider this variable:
-// var animals = [
-//   { name: "Waffles", type: "dog", age: 12 },
-//   { name: "Fluffy", type: "cat", age: 14 },
-//   { name: "Spelunky", type: "dog", age: 4 },
-//   { name: "Hank", type: "cat", age: 11 },
-// ]
+var animals = [
+  { name: "Waffles", type: "dog", age: 12 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 },
+]
 // Create a function that takes in any array of objects and returns a new array with only those of type cat.
 // Using the same array of objects above. Create a function that returns a new array with only the names of the animals.
+const myFilter = (object) =>{
+  return object.filter(value =>{
+      return value.type === "cat"
+  })
+}
+
+console.log(myFilter(animals));
 // Consider this variable:
 // let author = {
 //     name: "H. G. Wells",
