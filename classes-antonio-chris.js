7e7018ad77e6e5f96e7a927e578c72842637a99e
kickeyhickey <irishmanhickey@gmail.com>
2021-12-13 12:43:53 -0800

    finished

--- classes-antonio-chris.js
+++ classes-antonio-chris.js
@@ -44,22 +44,12 @@ var regCoffee = new Coffee ("plain", 1, 2)
       this.flavor = flavor.toLowerCase()
       this.milkType = milkType
       this.shots = shots
+
     }
 
     latteProfile(){
       return `A ${this.flavor} Latte with ${this.milkType}, ${this.numShots()}`
     }
-
-    // creams(){
-    //   if(this.cream > 1){
-    //     return `${this.cream} creams`
-    //   } else {
-    //     return `${this.cream} cream`
-    //   }
-    // }
-    // milkType(){
-      // return `${milkType}`
-    // }
 
     numShots(){
       if(this.shots > 1){
@@ -78,10 +68,48 @@ var regCoffee = new Coffee ("plain", 1, 2)
   //
 
   // Write the code that makes a regular, single shot latte. Then, log the latte's profile.
-  //
+   var regLatte = new Latte ("regular", "almond milk", 1)
+    // console.log(regLatte.latteProfile());
   // Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
-  //
+   var hazelnutLatte = new Latte ("hazelnut", "almond milk", 2)
+  console.log(hazelnutLatte.latteProfile());
+  
+  
+  
   // Volume of a Cylinder
   // Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
   //
-  // Write the code that creates three unique cylinder objects
+  class Cylinder{
+    constructor(radius, height){
+      this.radius = radius
+      this.height = height
+      
+
+    }
+     calc(){
+      if(this.radius > 0 && this.height > 0){
+        return (Math.PI * this.radius ** 2 * this.height).toFixed(4)
+      } else {
+        return "this is not a circle"
+      }
+     }
+  }
+  
+  var newCylinder = new Cylinder(45, 57)
+  // console.log(newCylinder.calc());
+  // // Write the code that creates three unique cylinder objects
+  
+  var secondCylinder = new Cylinder (11,3)
+  // console.log(secondCylinder.calc());
+
+  var thirdCylinder = new Cylinder(5,9)
+  // console.log(thirdCylinder.calc());
+
+  var newArr = [newCylinder, secondCylinder, thirdCylinder]
+
+  var combinedCylinder = (array) =>{
+   return array.map(value =>{
+      return parseFloat(value.calc())
+    })
+  }
+console.log(combinedCylinder(newArr));
