0c75e70053ba069b65d764f69a198d6d6a4bd9fe
Chava888 <salvador_tena@hotmail.com>
2021-12-13 16:24:44 -0800

    completed challenges

--- inheritance-deon-salvador.js
+++ inheritance-deon-salvador.js
@@ -1,46 +1,78 @@
-class car{
-    constructor(myCar, modelCar, carYear){
-    this.car = myCar
-    this.model = modelCar
-    this.year = carYear
-    this.wheels = 4
-    this.lights = false
+class car {
+    constructor(myCar, modelCar, carYear, acceleratorUnit, brakingUnit) {
+        this.car = myCar
+        this.model = modelCar
+        this.year = carYear
+        this.wheels = 4
+        this.lights = false
+        this.turningLights = false
+        this.speed = 0;
+        this.acceleratorUnit = acceleratorUnit
+        this.brakingUnit = brakingUnit
+
     }
-    turnOnLights (){
+    turnOnLights() {
         return this.lights = !this.lights
+    }
+    turningLight() {
+        return this.turningLights = !this.turningLights
+    }
+
+    carInfo() {
+        return `This car is a ${this.year} ${this.car}, this model ${this.model} has ${this.wheels} wheels.`//cartype, model, year, numberOfWheels
+    }
+    accelerator() {
+        return this.speed = this.speed += this.acceleratorUnit
+    }
+    braking() {
+        return this.speed = this.speed -= this.brakingUnit
     }
 }
 
-let myCar = new car("Sport", "Hatchback", 2025)
+let myCar = new car("Sport", "Hatchback", 2025, 6, 2)
 myCar.turnOnLights()
 myCar.turnOnLights()
-
+myCar.accelerator()
+myCar.braking()
 console.log(myCar)
 
 class Tesla extends car {
-    constructor(myCar, modelCar, carYear){
-        super(myCar, modelCar, carYear)
+    constructor(myCar, modelCar, carYear, acceleratorUnit, brakingUnit) {
+        super(myCar, modelCar, carYear, acceleratorUnit, brakingUnit)
+
     }
+
 }
 
-let myTesla = new Tesla( "Sport", "y", 2021)
+let myTesla = new Tesla("Tesla", "y", 2021, 10, 7)
+console.log(myTesla.carInfo())
+myTesla.accelerator()
+myTesla.braking()
 console.log(myTesla)
 
 class Toyota extends car {
-    constructor(myCar, modelCar, carYear){
-        super(myCar, modelCar, carYear)
+    constructor(myCar, modelCar, carYear, acceleratorUnit, brakingUnit) {
+        super(myCar, modelCar, carYear, acceleratorUnit, brakingUnit)
     }
+
 }
 
-let myToyota = new Toyota( "Sport", "Tacoma", 2017)
+let myToyota = new Toyota("Toyota", "Tacoma", 2017, 5, 2)
+myToyota.accelerator()
+myToyota.braking()
 console.log(myToyota)
 
 class Volkswagon extends car {
-    constructor(myCar, modelCar, carYear){
-        super(myCar, modelCar, carYear)
+    constructor(myCar, modelCar, carYear, acceleratorUnit, brakingUnit) {
+        super(myCar, modelCar, carYear, acceleratorUnit, brakingUnit)
     }
+
 }
 
-let myVolkswagon = new Volkswagon( "GTI", "Golf", 2018)
+let myVolkswagon = new Volkswagon("Volkswagon", "Golf", 2018, 7, 5)
+myVolkswagon.accelerator()
+myVolkswagon.braking()
 console.log(myVolkswagon)
+
+
 
